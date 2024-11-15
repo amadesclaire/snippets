4af2436a288687333d30076a6f3313c3b71086d1
Alexandre Madesclaire <alxm95@gmail.com>
2024-11-15 14:52:14 +0000

    pattern matching

--- src/snippets/patternMatching.ts
+++ src/snippets/patternMatching.ts
@@ -1,3 +1,4 @@
+// String Matching
 class Matcher {
   private value: string;
   private matched: boolean = false;
@@ -23,4 +24,32 @@ class Matcher {
 
 export function match(value: string) {
   return new Matcher(value);
+}
+
+// With match condition
+class ConditionMatcher<T> {
+  private value: T;
+  private matched: boolean = false;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  with(matchCondition: (value: T) => boolean, action: () => void): this {
+    if (!this.matched && matchCondition(this.value)) {
+      action();
+      this.matched = true;
+    }
+    return this;
+  }
+
+  otherwise(action: () => void): void {
+    if (!this.matched) {
+      action();
+    }
+  }
+}
+
+export function cMatch<T>(value: T) {
+  return new ConditionMatcher(value);
 }
